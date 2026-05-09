ed48450d2dca82f56d447012114c69bf453d71ea
hen-chacha <henpoesh@gmail.com>
2026-05-10 02:45:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -227,4 +227,5 @@ async def download_video(background_tasks: BackgroundTasks, url: str = Form(...)
         return {"error": f"Ошибка сервера: {str(e)}"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=7860)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
